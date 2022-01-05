c4876979e5d047b945eaf929f7fd2a98bcc55e3e
Ace <aevancho@me.com>
2022-01-05 10:04:05 -0500

    first commit. project setup

---
+++ src/Components/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from '../Styles/logo.svg';
+import '../Styles/App.css'
+
+// FOR DEV TOOLS
+import { connectReduxDevtools } from 'mst-middlewares';
+import { types, getSnapshot } from 'mobx-state-tree';
+import makeInspectable from 'mobx-devtools-mst';
+const remotedev = require('remotedev');
+
+const ToDoStore = types
+  .model({
+    id: types.number,
+  })
+  .views((self) => ({
+    // code here
+  }))
+  .actions((self) => ({
+    // code here
+  }));
+
+const store = makeInspectable(ToDoStore.create({id: 1}));
+connectReduxDevtools(remotedev, store);
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
